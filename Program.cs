d508f78639101811c64e4e0cb648532b4b953898
Pip Matthews <pip.matthews+github@gmail.com>
2023-03-21 21:19:27 -0400

    created the dino class and the add function

--- Program.cs
+++ Program.cs
@@ -1,12 +1,100 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace JurassicPark
 {
-  class Program
-  {
-    static void Main(string[] args)
+
+    class Dinosaur
     {
-      Console.WriteLine("Welcome to C#");
+        public string Name { get; set; }
+        public string DietType { get; set; }
+        public DateTime WhenAcquired { get; set; } = DateTime.Now;
+        public int Weight { get; set; }
+        public int EnclosureNumber { get; set; }
+
+        public string Description()
+        {
+            string newDescript = ("Your Dino's name is " + Name + " and it's diet type is " + DietType + ". It weighs " + Weight + " and it's kept in enclosure number " + EnclosureNumber + ".");
+            return newDescript;
+        }
     }
-  }
+    class Program
+    {
+        static string PromptForString(string prompt)
+        {
+            Console.Write(prompt);
+            var userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int PromptForInteger(string prompt)
+        {
+            Console.Write(prompt);
+            int userInput;
+            var isThisGoodInput = Int32.TryParse(Console.ReadLine(), out userInput);
+            if (isThisGoodInput)
+            {
+                return userInput;
+            }
+            else
+            {
+                Console.WriteLine("Ah Ah Ah, that isn't a valid input, I'm using 0 as your answer.");
+                return 0;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("    Welcome to Jurassic Park      ");
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+
+            var dinosaurs = new List<Dinosaur>();
+
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine("(V)view, (A)add, (R)remove, (T)transfer, (S)summary or (Q)quit");
+            Console.WriteLine();
+            string response = Console.ReadLine();
+            if (response == "V" || response == "v")
+            {
+
+            }
+            else if (response == "A" || response == "a")
+            {
+                var dinoToAdd = new Dinosaur();
+                dinoToAdd.Name = PromptForString("What is the name of your dinosaur? ");
+                dinoToAdd.DietType = PromptForString("Is your dinosaur a herbivore or a carnivore? ");
+                dinoToAdd.Weight = PromptForInteger("What is the weight of your dinosaur? ");
+                dinoToAdd.EnclosureNumber = PromptForInteger("Which enclosure number is your dinosaur in? ");
+                dinosaurs.Add(dinoToAdd);
+                Console.WriteLine($"{dinoToAdd.Description()}");
+                Console.WriteLine();
+                Console.WriteLine("Dino Added!");
+                Console.WriteLine();
+
+            }
+            else if (response == "R" || response == "r")
+            {
+
+            }
+            else if (response == "T" || response == "t")
+            {
+
+            }
+            else if (response == "S" || response == "s")
+            {
+
+            }
+            else if (response == "Q" || response == "q")
+            {
+
+            }
+
+
+
+
+        }
+    }
 }
